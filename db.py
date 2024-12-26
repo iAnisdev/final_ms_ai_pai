5f2e217de938881e1313299cda93db9805043576
Anis <me.anis.dev@gmail.com>
2024-12-26 03:27:55 +0000

    :zap: fetch historical data for BTC, ETH, BNB, and SOL using process_data function

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import asyncio
 import asyncpg
 import os
 from dotenv import load_dotenv
+from shared.process_data import process_data
 
 load_dotenv()
 
@@ -15,6 +16,12 @@ async def main():
         port=os.getenv("DB_PORT"),
     )
     print("Connection established successfully" if connection else "Connection failed")
+
+    # fetch data for btc, eth , bnb and sol for 5 years
+    await process_data(connection, "BTCUSDT", "bitcoin", '1d', 5)
+    await process_data(connection, "ETHUSDT", "ethereum", '1d', 5)
+    await process_data(connection, "BNBUSDT", "binance", '1d', 5)
+    await process_data(connection, "SOLUSDT", "solana", '1d', 5)
 
 
 if __name__ == "__main__":
